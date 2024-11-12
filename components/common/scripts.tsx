37c7be578cec7f7054cbe9d84cf452ecf8c9622c
HoanqDucAnh <hoangoggy2601@gmail.com>
2024-11-12 22:08:47 +0700

    fix: rollback, hardcoded total view to fix connection to database concurrently

--- components/common/scripts.tsx
+++ components/common/scripts.tsx
@@ -1,79 +1,97 @@
 import Script from "next/script";
 import React, { useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, get, child, set, onValue } from "firebase/database";
+import {
+	getDatabase,
+	goOnline,
+	goOffline,
+	get,
+	child,
+	ref,
+	set,
+} from "firebase/database";
 
-const firebaseConfig = {
-	apiKey: "AIzaSyC7Bd9cOnlhZFTrxMZVbVzaRa9opnSnc4k",
-	authDomain: "bminh-porfolio-view-counter.firebaseapp.com",
-	projectId: "bminh-porfolio-view-counter",
-	storageBucket: "bminh-porfolio-view-counter.firebasestorage.app",
-	messagingSenderId: "352556857178",
-	appId: "1:352556857178:web:e0671a9649fa0cbd4c6563",
-	measurementId: "G-6T2HTBS4WQ",
-};
+// const firebaseConfig = {
+// 	apiKey: "AIzaSyC7Bd9cOnlhZFTrxMZVbVzaRa9opnSnc4k",
+// 	authDomain: "bminh-porfolio-view-counter.firebaseapp.com",
+// 	projectId: "bminh-porfolio-view-counter",
+// 	storageBucket: "bminh-porfolio-view-counter.firebasestorage.app",
+// 	messagingSenderId: "352556857178",
+// 	appId: "1:352556857178:web:e0671a9649fa0cbd4c6563",
+// 	measurementId: "G-6T2HTBS4WQ",
+// };
 
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+// const app = initializeApp(firebaseConfig);
+// const database = getDatabase(app);
 
-interface IpInfo {
-	ip: string;
-	country: string;
-	city: string;
-}
+// interface IpInfo {
+// 	ip: string;
+// 	country: string;
+// 	city: string;
+// }
 
-interface UserDetails {
-	ip: string;
-}
+// interface UserDetails {
+// 	ip: string;
+// 	visitTime: number;
+// }
 
-const countview = async (
-	setViewCount: React.Dispatch<React.SetStateAction<number>>
-): Promise<void> => {
-	try {
-		const ipinfo: IpInfo = await fetch("https://api.ipify.org?format=json", {
-			method: "GET",
-		}).then((response) => {
-			if (!response.ok) {
-				throw new Error(`HTTP error! status: ${response.status}`);
-			}
-			return response.json();
-		});
-		const { ip: userIp } = ipinfo;
-		const userIpString = userIp.replace(/\./g, "x");
+// const countview = async (
+// 	setViewCount: React.Dispatch<React.SetStateAction<number>>
+// ): Promise<void> => {
+// 	try {
+// 		const ipinfo: IpInfo = await fetch("https://api.ipify.org?format=json", {
+// 			method: "GET",
+// 		}).then((response) => {
+// 			if (!response.ok) {
+// 				throw new Error(`HTTP error! status: ${response.status}`);
+// 			}
+// 			return response.json();
+// 		});
+// 		const { ip: userIp } = ipinfo;
+// 		const userIpString = userIp.replace(/\./g, "x");
 
-		const viewsRef = ref(database, "views");
-		const snapshot = await get(viewsRef);
+// 		goOnline(database);
 
-		setViewCount(snapshot.size);
+// 		const viewsRef = ref(database, "views");
+// 		const snapshot = await get(viewsRef);
 
-		if (snapshot.hasChild(userIpString)) {
-			console.log("View Will Not Count, This is a returning user");
-		} else {
-			const userDetails: UserDetails = { ip: userIp };
-			await set(child(viewsRef, userIpString), userDetails);
-			console.log("View counted, looks like a new user.");
-		}
-	} catch (error) {
-		console.error(
-			"Error fetching user data or interacting with Firebase:",
-			error
-		);
-	}
-};
+// 		setViewCount(snapshot.size);
+
+// 		const currentTime = Date.now();
+// 		const oneDayInMillis = 24 * 60 * 60 * 1000;
+
+// 		if (snapshot.hasChild(userIpString)) {
+// 			const userDetails: UserDetails = snapshot.child(userIpString).val();
+// 			if (currentTime - userDetails.visitTime < oneDayInMillis) {
+// 				console.log(
+// 					"View Will Not Count, welcome back! Hope you are enjoying my portfolio!"
+// 				);
+// 				return;
+// 			}
+// 		}
+
+// 		const userDetails: UserDetails = { ip: userIp, visitTime: currentTime };
+// 		await set(child(viewsRef, userIpString), userDetails);
+// 		console.log(
+// 			"View counted, hi new user! Welcome to my portfolio, hope you like it!"
+// 		);
+
+// 		goOffline(database);
+// 	} catch (error) {
+// 		goOffline(database);
+// 		console.error(
+// 			"Error fetching user data or interacting with Firebase:",
+// 			error
+// 		);
+// 	}
+// };
 
 const Scripts: React.FC = () => {
-	const [viewCount, setViewCount] = useState(0);
+	// const [viewCount, setViewCount] = useState(0);
 
-	useEffect(() => {
-		const viewsRef = ref(database, "views");
-		const unsubscribe = onValue(viewsRef, (snapshot) => {
-			setViewCount(snapshot.size);
-		});
-
-		countview(setViewCount);
-
-		return () => unsubscribe();
-	}, []);
+	// useEffect(() => {
+	// 	countview(setViewCount);
+	// }, []);
 
 	const handleJumpToFirst = () => {
 		window.scrollTo(0, 0);
@@ -81,7 +99,7 @@ const Scripts: React.FC = () => {
 
 	return (
 		<div className="fixed right-5 bottom-5 flex items-center space-x-4">
-			<div className="text-3xl text-[#f27d0d]">Total Views: {viewCount}</div>
+			<div className="text-3xl text-[#f27d0d]">Total Views: 3</div>
 			<button
 				onClick={handleJumpToFirst}
 				className="flex items-center flex-col border-l-indigo-50 rounded-3xl"
