4036fd6787eb0bfd1adc8d174e82beb02bd6ab21
Mark Pham <minh.pham@insurify.com>
2025-07-17 17:41:38 -0400

    fix: article bug

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -134,7 +134,11 @@ const HeroSection = React.memo(() => {
 		const typed = initTypeAnimation(typedSpanElement);
 		initRevealAnimation(targetSection);
 
-		return typed.destroy;
+		return () => {
+			if (typed) {
+				typed.destroy();
+			}
+		};
 	}, [typedSpanElement, targetSection]);
 
 	const renderBackgroundImage = (): React.ReactNode => (
