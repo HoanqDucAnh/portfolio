c309e482a213b409f32e7281fa9b2563bcb9015f
Mark Pham <minh.pham@insurify.com>
2026-04-09 21:30:42 -0400

    feat: add GA4 and Microsoft Clarity analytics tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,41 @@
 
 import "../styles/globals.scss";
 import "../styles/article.css";
-// import "prismjs/themes/prism.css";
 
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />
+
+			{/* GA4 */}
+			<Script
+				src="https://www.googletagmanager.com/gtag/js?id=G-6T2HTBS4WQ"
+				strategy="afterInteractive"
+			/>
+			<Script id="ga4-init" strategy="afterInteractive">
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'G-6T2HTBS4WQ', { page_path: window.location.pathname });
+				`}
+			</Script>
+
+			{/* Microsoft Clarity */}
+			<Script id="clarity" strategy="afterInteractive">
+				{`
+					(function(c,l,a,r,i,t,y){
+						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+					})(window,document,"clarity","script","w99ronz0em");
+				`}
+			</Script>
+		</>
+	);
 }
 
 export default MyApp;
